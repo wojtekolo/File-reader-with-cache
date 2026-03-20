611402d21ffd8f19e541a98b3f58d5a4dfb9dc55
Wojciech Kołodziejczyk <281227@student.pwr.edu.pl>
2026-03-20 21:18:08 +0100

    refactor(lab03): implement using different languages Move files to `question` and `words` subdirectories

--- lab2/src/main/java/pl/wojtekolo/studia/file/FileCacher.java
+++ lab2/src/main/java/pl/wojtekolo/studia/file/FileCacher.java
@@ -4,9 +4,10 @@ import java.io.File;
 import java.lang.ref.WeakReference;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.WeakHashMap;
 
 public class FileCacher {
-    Map<String, WeakReference<ReadFileDTO>> cache = new HashMap<>();
+    Map<String, WeakReference<ReadFileDTO>> cache = new WeakHashMap<>();
 
     public ResponseDTO getData(File file, FileProcessingStrategy strategy){
         String path = file.getAbsolutePath();
